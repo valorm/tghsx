bf64f9bbfd1da3a7d1c271bfd8ab45898ec1fa38
valorm <i_amflake@outlook.com>
2025-07-10 00:31:15 +0000

    Configure backend for Render deployment

--- frontend/script/auth.js
+++ frontend/script/auth.js
@@ -6,7 +6,7 @@
  * ==================================================================================
  */
 
-const BACKEND_URL = 'http://127.0.0.1:8000';
+const BACKEND_URL = 'https://tghsx.onrender.com';
 let toastTimeout;
 
 /**
