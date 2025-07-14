e93a43a3e6cdc8da8106b746ec4259f9c5b11ddf
valorm <i_amflake@outlook.com>
2025-07-14 01:26:06 +0000

    rollback: remove contracts directory and related files

--- contracts/arguments-btc-usd.js
+++
@@ -1,6 +0,0 @@
-const { ethers } = require("ethers");
-module.exports = [
-  ethers.utils.parseUnits("117669.00", 8),
-  8,
-  "BTC/USD Mock"
-];
--- contracts/arguments-eth-btc.js
+++
@@ -1,6 +0,0 @@
-const { ethers } = require("ethers");
-module.exports = [
-  ethers.utils.parseUnits("0.025", 8), 
-  8, 
-  "ETH/BTC Mock"
-];
